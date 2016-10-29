5ce24c4c1e142fa8c977bd32181cf6bc38e1862a
wellwind <wellwind@mail2000.com.tw>
2016-10-29 23:22:44 +0800

    set typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,4 +2,8 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
+/// <reference path="../node_modules/firebase/firebase.d.ts" />
+
 declare var System: any;
+declare var require: any;
+declare var module: any;
