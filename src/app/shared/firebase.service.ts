3b561717500b234c0779c38776729e3c64c92f2d
wellwind <wellwind@mail2000.com.tw>
2016-11-01 17:08:11 +0800

    add canActivate to firebase service to check auth

--- src/app/shared/firebase.service.ts
+++ src/app/shared/firebase.service.ts
@@ -1,13 +1,17 @@
 import { AngularFire, FirebaseAuthState } from 'angularfire2';
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/first';
+import 'rxjs/Rx';
 
 @Injectable()
-export class FirebaseService {
+export class FirebaseService implements CanActivate {
 
-  constructor(public angularFire: AngularFire) {
+  constructor(public angularFire: AngularFire, public router: Router) {
   }
 
-  getAuthResult(){
+  getAuthResult() {
     return this.angularFire.auth;
   }
 
@@ -17,5 +21,19 @@ export class FirebaseService {
 
   logout() {
     this.angularFire.auth.logout();
+    this.router.navigate(['/']);
   }
+
+  canActivate() {
+    return this.angularFire.auth
+      .take(1)
+      .map((authState: FirebaseAuthState) => !!authState)
+      .do(authenticated => {
+        if (!authenticated) {
+          this.login();
+        }
+        return false;
+      });
+  }
+
 }
