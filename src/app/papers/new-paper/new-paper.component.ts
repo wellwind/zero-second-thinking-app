c711ca8f5a2d9b3d9b7049175d2c23e93323fa3f
wellwind <wellwind@mail2000.com.tw>
2016-11-18 23:27:07 +0800

    add new paper from component

--- src/app/papers/new-paper/new-paper.component.ts
+++ src/app/papers/new-paper/new-paper.component.ts
@@ -1,5 +1,7 @@
+import { FirebaseService } from '../../shared/firebase.service';
 import { PaperContent } from './../../shared/interfaces/paper-content';
-import { Component, OnInit, Renderer, ElementRef } from '@angular/core';
+import { Component, OnInit, Renderer, ElementRef, ChangeDetectorRef } from '@angular/core';
+import { database } from 'firebase';
 
 @Component({
   selector: 'app-new-paper',
@@ -9,17 +11,40 @@ import { Component, OnInit, Renderer, ElementRef } from '@angular/core';
 export class NewPaperComponent implements OnInit {
 
   content: PaperContent;
+  tagString: string;
 
-  constructor(private renderer: Renderer, private elementRef: ElementRef) { }
+  constructor(
+    private renderer: Renderer,
+    private elementRef: ElementRef,
+    private changeDetectorRef: ChangeDetectorRef,
+    private firebaseService: FirebaseService) {
+  }
 
   ngOnInit() {
     this.content = {
       title: '',
-      lines: []
+      lines: [],
+      category: '',
+      tags: []
     };
+    this.tagString = '';
   }
 
   newLine() {
     this.content.lines.push('');
+    this.changeDetectorRef.detectChanges();
+  }
+
+  save() {
+    this.content.lines = this.content.lines.filter(val => val.trim() !== '');
+    this.content.tags = (this.tagString.trim() !== '') ? this.tagString.split(',').map(val => val.trim()) : [];
+    console.log(this.content);
+    (this.firebaseService.createNewPaper(this.content) as database.ThenableReference)
+      .then(() => {
+
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
--- src/app/papers/papers.module.ts
+++ src/app/papers/papers.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -8,6 +9,7 @@ import { NewPaperComponent } from './new-paper/new-paper.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     PapersRoutingModule,
     SharedModule
   ],
