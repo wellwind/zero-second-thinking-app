93f7a44c2f283e988072a46dd83e3b9c7624f4f4
wellwind <wellwind@mail2000.com.tw>
2016-12-31 22:26:24 +0800

    query paper basic data

--- src/app/papers/view/view.component.ts
+++ src/app/papers/view/view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FirebaseService } from '../../shared/firebase.service';
+import { PaperContent } from '../../shared/interfaces/paper-content';
 
 @Component({
   selector: 'app-view',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  paperData: PaperContent;
+
+  constructor(private route: ActivatedRoute, private firebaseService: FirebaseService) { }
 
   ngOnInit() {
+    this.route.params.take(1).toPromise().then(param => {
+      return this.firebaseService.queryPaper(param['key']);
+    }).then(paper => {
+      console.log(paper);
+      this.paperData = paper as PaperContent;
+    });
   }
 
 }
