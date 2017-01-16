88b117c83c3faa1957dafa09445c22691a7c9363
wellwind <wellwind@mail2000.com.tw>
2017-01-16 23:52:53 +0800

    new gh-pages version

--- src/app/config/firebase.config.ts
+++
@@ -1,7 +0,0 @@
-export var FirebaseConfig = {
-    apiKey: 'AIzaSyDgurHRFJ3G9-qeG8o8z_by1GqUoeeigT8',
-    authDomain: 'zero-second-thinking.firebaseapp.com',
-    databaseURL: 'https://zero-second-thinking.firebaseio.com',
-    storageBucket: 'zero-second-thinking.appspot.com',
-    messagingSenderId: '357330617435'
-};
